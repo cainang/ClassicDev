aa40c26b3a238336ba851c7e6a9adeae7fdcd5bc
cainang <40045303+cainang@users.noreply.github.com>
2021-12-14 14:58:59 -0300

    Update QuizSection.js

--- src/QuizSection.js
+++ src/QuizSection.js
@@ -111,7 +111,7 @@ const QuizSection = ({category}) => {
         //let url = "https://opentdb.com/api.php?amount=10&category="+cat+"&difficulty=medium&type=multiple";
         
         var params = encodeURI(category)
-        let url = 'https://classicdev.loca.lt/quests?dificudade=' + params;
+        let url = 'https://bc1e-2804-d4b-7ad2-5000-9ae7-43ff-fef4-e7a6.ngrok.io/quests?dificudade=' + params;
         let source = axios.CancelToken.source();
 
         axios.get(url)
